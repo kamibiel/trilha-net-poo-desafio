711e2fd17c676164d1662ceacd4de967988436b5
Gabriel Bonifácio Oliveira <gabriel.o.bonifacio@gmail.com>
2024-09-29 10:44:21 -0300

    Desafio completo

--- Models/Smartphone.cs
+++ Models/Smartphone.cs
@@ -4,19 +4,25 @@ namespace DesafioPOO.Models
     {
         public string Numero { get; set; }
         // TODO: Implementar as propriedades faltantes de acordo com o diagrama
+        public string Modelo { get; private set; }
+        public string IMEI { get; private set; }
+        public int Memoria { get; private set; }
 
-        public Smartphone(string numero)
+        public Smartphone(string numero, string modelo, string imei, int memoria)
         {
             Numero = numero;
             // TODO: Passar os parâmetros do construtor para as propriedades
+            Modelo = modelo;
+            IMEI = imei;
+            Memoria = memoria;
         }
 
-        public void Ligar()
+        public virtual void Ligar()
         {
             Console.WriteLine("Ligando...");
         }
 
-        public void ReceberLigacao()
+        public virtual void ReceberLigacao()
         {
             Console.WriteLine("Recebendo ligação...");
         }
--- Program.cs
+++ Program.cs
@@ -1,3 +1,18 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("-------------------");
+Console.WriteLine("Smartphone Nokia");
+Console.WriteLine("-------------------");
+Smartphone nokia = new Nokia(numero:"21964291278", modelo:"Modelo 1", imei:"111111111", memoria:128);
+nokia.Ligar();
+nokia.InstalarAplicativo("WhatsApp");
+
+Console.WriteLine("\n");
+
+Console.WriteLine("-------------------");
+Console.WriteLine("Smartphone Iphone");
+Console.WriteLine("-------------------");
+Smartphone iphone = new Iphone(numero:"21964291278", modelo:"Modelo 2", imei:"2222222222222", memoria:256);
+iphone.Ligar();
+iphone.InstalarAplicativo("Telegram");
